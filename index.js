2a6e9763beb3a882239b856f47ac48f2279d0569
arthurlub <82172990+arthurlub@users.noreply.github.com>
2022-07-19 21:13:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World! YAY☺ ");
+  res.send("Hello World! YAY☺  ");
 });
 
 app.listen(5000, () => {
